7f0ef4d8f6256d0fa342d2597ee49375dbcb7e4b
Gwinfox <Gwinnfox@gmail.com>
2026-03-07 11:09:32 +0300

    smallfixes

--- brainpocket/src/bll/Hooks/ProfilePageHooks/useProfileData.tsx
+++ brainpocket/src/bll/Hooks/ProfilePageHooks/useProfileData.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { profileAPI } from "../../../dal/api";
 import type { Profile } from "../../types/profileTypes";
 import type { UserData } from "../../types/appTypes";
